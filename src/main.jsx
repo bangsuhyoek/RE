0938d1ed5f743fe3781552028fcd747fc7d589df
syemk999-collab <syemk999@gmail.com>
2026-09-06 02:16:44 +0900

    Load dashboard reference theme after existing web theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./web-theme.css";
+import "./dashboard-theme.css";
 
 createRoot(document.getElementById("root")).render(<App />);
